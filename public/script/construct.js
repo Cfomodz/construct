016e48c262c5d598b6d0b337f2e1101db560e851
Jason Huggins <jrhuggins@gmail.com>
2020-10-05 12:40:36 -0500

    A subtle typo fix.

--- public/script/construct.js
+++ public/script/construct.js
@@ -1,7 +1,7 @@
 // "This is the Construct. It's our loading program. We
 // can load anything... From clothing to equipment,
 // weapons, training simulations; anything we need."
-// ―Morpheus
+// —Morpheus
 
 
 
